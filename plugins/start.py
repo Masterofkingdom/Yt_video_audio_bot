faa94130c4175d9d370dd61993a3a27683ee1bbd
Masterofkingdom <87687529+Masterofkingdom@users.noreply.github.com>
2022-04-21 11:00:36 +0530

    Create start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
-        [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+        [InlineKeyboardButton("⚡ Our Channel ⚡", url="https://t.me/kingdom_family_chanel"),
+         InlineKeyboardButton("⚡ Our Group ⚡", url="https://t.me/kingdom_family")]
+        [InlineKeyboardButton("⚡ Share Channel ⚡", url="https://telegram.me/share/url?url=t.me/kingdom_family_chanel")],
+        [InlineKeyboardButton("⚡ Share Group ⚡", url="https://telegram.me/share/url?url=t.me/kingdom_family")],
+        [InlineKeyboardButton("⚡ kathasayura ⚡", url="https://t.me/kathasayura"),
+         InlineKeyboardButton("⚡ stickers ⚡", url="https://t.me/kingdom_family_sticker")],
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hi 👋 <b>{message.from_user.first_name}</b>\nThis is **kingdo family product** This bot can download You tube video and audio \nhelp For more \n\n\n 𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 :- @kingdom_family_chanel"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
