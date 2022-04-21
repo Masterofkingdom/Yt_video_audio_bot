257e9d14b697a05d54a752737b95d88a5c4e143d
Masterofkingdom <87687529+Masterofkingdom@users.noreply.github.com>
2022-04-21 10:45:35 +0530

    Create help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"This is **kingdom family product** now send  (No playlist) send a you tube url and select the catagori of you want"
     await message.reply_text(helptxt)
